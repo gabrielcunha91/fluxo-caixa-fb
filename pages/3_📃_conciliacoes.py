b2b42fa75695466c519e5016456c39bc464f49a9
gabrielcunha91 <128188960+gabrielcunha91@users.noreply.github.com>
2024-04-12 13:54:21 +0000

    msg

--- pages/3_📃_conciliacoes.py
+++ pages/3_📃_conciliacoes.py
@@ -57,10 +57,10 @@ st.divider()
 st.markdown("Receitas Extraordinárias")
 
 df_receitas_extraord = st.session_state["receitas_extraord"]
-df_receitas_extraord
+df_receitas_extraord_loja = df_receitas_extraord[df_receitas_extraord['ID_Loja'] == id_loja]
+df_receitas_extraord_loja
 # Chama a função para atualizar o arquivo Excel
 if st.button('Atualizar Receitas Extraord'):
-    df_receitas_extraord_loja = df_receitas_extraord[df_receitas_extraord['ID_Loja'] == id_loja]
     sheet_name_receitas_extraord = 'df_receitas_extraord'
     export_to_excel(df_receitas_extraord_loja, sheet_name_receitas_extraord, excel_filename)
     st.success('Arquivo atualizado com sucesso!')
@@ -69,10 +69,10 @@ st.divider()
 st.markdown("View Parcelamentos Agrupados - Receitas Extraord")
 
 df_view_parc_agrup = st.session_state["view_parc_agrup"]
-df_view_parc_agrup
+df_view_parc_loja = df_view_parc_agrup[df_view_parc_agrup['ID_Loja'] == id_loja]
+df_view_parc_loja
 # Chama a função para atualizar o arquivo Excel
 if st.button('Atualizar View Parcelamentos Receitas Extraord'):
-    df_view_parc_loja = df_view_parc_agrup[df_view_parc_agrup['ID_Loja'] == id_loja]
     sheet_name_view_parc_agrup = 'view_parc_agrup'
     export_to_excel(df_view_parc_loja, sheet_name_view_parc_agrup, excel_filename)
     st.success('Arquivo atualizado com sucesso!')
@@ -81,10 +81,10 @@ st.divider()
 st.markdown("Custos BlueMe Sem Parcelamento")
 
 df_custos_blueme_sem_parcelamento = st.session_state["custos_blueme_sem_parcelamento"]
-df_custos_blueme_sem_parcelamento
+df_custos_blueme_sem_parcelamento_loja = df_custos_blueme_sem_parcelamento[df_custos_blueme_sem_parcelamento['ID_Loja'] == id_loja]
+df_custos_blueme_sem_parcelamento_loja
 # Chama a função para atualizar o arquivo Excel
 if st.button('Atualizar Custos BlueMe Sem Parcelamento'):
-    df_custos_blueme_sem_parcelamento_loja = df_custos_blueme_sem_parcelamento[df_custos_blueme_sem_parcelamento['ID_Loja'] == id_loja]
     sheet_name_custos_blueme_sem_parcelamento = 'df_blueme_sem_parcelamento'
     export_to_excel(df_custos_blueme_sem_parcelamento_loja, sheet_name_custos_blueme_sem_parcelamento, excel_filename)
     st.success('Arquivo atualizado com sucesso!')
@@ -93,10 +93,10 @@ st.divider()
 st.markdown("Custos BlueMe Com Parcelamento")
 
 df_custos_blueme_com_parcelamento = st.session_state["custos_blueme_com_parcelamento"]
-df_custos_blueme_com_parcelamento
+df_custos_blueme_com_parcelamento_loja = df_custos_blueme_com_parcelamento[df_custos_blueme_com_parcelamento['ID_Loja'] == id_loja]
+df_custos_blueme_com_parcelamento_loja
 # Chama a função para atualizar o arquivo Excel
 if st.button('Atualizar Custos BlueMe Com Parcelamento'):
-    df_custos_blueme_com_parcelamento_loja = df_custos_blueme_com_parcelamento[df_custos_blueme_com_parcelamento['ID_Loja'] == id_loja]
     sheet_name_custos_blueme_com_parcelamento = 'df_blueme_com_parcelamento'
     export_to_excel(df_custos_blueme_com_parcelamento_loja, sheet_name_custos_blueme_com_parcelamento, excel_filename)
     st.success('Arquivo atualizado com sucesso!')
@@ -105,10 +105,10 @@ st.divider()
 st.markdown("Extratos Bancários")
 
 df_extratos = st.session_state["extratos_bancarios"]
-df_extratos
+df_extratos_loja = df_extratos[df_extratos['ID_Loja'] == id_loja]
+df_extratos_loja
 # Chama a função para atualizar o arquivo Excel
 if st.button('Atualizar Extratos'):
-    df_extratos_loja = df_extratos[df_extratos['ID_Loja'] == id_loja]
     sheet_name_extratos = 'df_extratos'
     export_to_excel(df_extratos_loja, sheet_name_extratos, excel_filename)
     st.success('Arquivo atualizado com sucesso!')
@@ -117,10 +117,10 @@ st.divider()
 st.markdown("Mutuos")
 
 df_mutuos = st.session_state["mutuos"]
-df_mutuos
+df_mutuos_loja = df_mutuos[((df_mutuos['ID_Loja_Saida'] == id_loja) | (df_mutuos['ID_Loja_Entrada'] == id_loja))]
+df_mutuos_loja
 # Chama a função para atualizar o arquivo Excel
 if st.button('Atualizar Mutuos'):
-    df_mutuos_loja = df_mutuos[((df_mutuos['ID_Loja_Saida'] == id_loja) | (df_mutuos['ID_Loja_Entrada'] == id_loja))]
     df_mutuos_loja['Valor_Entrada'] = df_mutuos_loja.apply(lambda row: row['Valor'] if row['ID_Loja_Entrada'] == id_loja else 0, axis=1)
     df_mutuos_loja['Valor_Saida'] = df_mutuos_loja.apply(lambda row: row['Valor'] if row['ID_Loja_Saida'] == id_loja else 0, axis=1)
     df_mutuos_loja = df_mutuos_loja.drop('Valor', axis=1)
