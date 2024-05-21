aa62b998d7277606cdaba994f926f9dd9478a44a
gabrielcunha91 <128188960+gabrielcunha91@users.noreply.github.com>
2024-05-21 13:33:15 +0000

    msg

--- pages/3_📃_conciliacoes.py
+++ pages/3_📃_conciliacoes.py
@@ -22,6 +22,22 @@ id_loja = mapeamento_lojas[loja]
 
 st.write(id_loja)
 
+# def export_to_excel(df, sheet_name, excel_filename):
+#     if os.path.exists(excel_filename):
+#         wb = openpyxl.load_workbook(excel_filename)
+#     else:
+#         wb = openpyxl.Workbook()
+
+#     if sheet_name in wb.sheetnames:
+#         wb.remove(wb[sheet_name])
+
+#     ws = wb.create_sheet(title=sheet_name)
+#     for r_idx, row in enumerate(df.iterrows(), start=1):
+#         for c_idx, value in enumerate(row[1], start=1):
+#             ws.cell(row=r_idx, column=c_idx, value=value)
+
+#     wb.save(excel_filename)
+
 def export_to_excel(df, sheet_name, excel_filename):
     if os.path.exists(excel_filename):
         wb = openpyxl.load_workbook(excel_filename)
@@ -32,11 +48,18 @@ def export_to_excel(df, sheet_name, excel_filename):
         wb.remove(wb[sheet_name])
 
     ws = wb.create_sheet(title=sheet_name)
-    for r_idx, row in enumerate(df.iterrows(), start=1):
-        for c_idx, value in enumerate(row[1], start=1):
-            ws.cell(row=r_idx, column=c_idx, value=value)
+    
+    # Escrever os cabeçalhos
+    for col_idx, column_title in enumerate(df.columns, start=1):
+        ws.cell(row=1, column=col_idx, value=column_title)
+    
+    # Escrever os dados
+    for row_idx, row in enumerate(df.itertuples(index=False, name=None), start=2):
+        for col_idx, value in enumerate(row, start=1):
+            ws.cell(row=row_idx, column=col_idx, value=value)
 
     wb.save(excel_filename)
+
 
 # Definindo o nome do arquivo excel
 excel_filename = 'Conciliacao_FB.xlsx'
